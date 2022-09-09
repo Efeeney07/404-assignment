dcf6441a3a204e307b410e4577df9cb35fa3dce8
Efeeney07 <89642750+Efeeney07@users.noreply.github.com>
2022-09-09 18:30:52 -0400

    should work

---
+++ The Bit/src/BitInterface.java
@@ -0,0 +1,13 @@
+
+public interface BitInterface {
+
+	void set(boolean v);
+	void toggle();
+	void set();
+	void clear();
+	boolean getValue();
+	Bit bitAnd(Bit b);
+	Bit bitOr(Bit b);
+	Bit bitXor(Bit b);
+	Bit bitNot();
+}
